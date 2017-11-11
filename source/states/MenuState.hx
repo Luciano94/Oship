f5b6219a1ad33229dd2b4ff167f8f10e728dba77
Unknown <tmugetti@gmail.com>
2017-11-11 13:56:26 -0300

    weas del menu

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -1,18 +1,25 @@
 package source.states;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.FlxG;
+import AssetPaths;
 
 class MenuState extends FlxState
 {
 	private var title:FlxText;
 	private var playButton:FlxButton;
+	private var fondo:FlxSprite;
 	
 	override public function create():Void
 	{
 		super.create();
+		fondo = new FlxSprite();
+		fondo.loadGraphic(AssetPaths.StartMenu__png);
+		add(fondo); //Tiene un zoom que corregir. No muestra el asset correctamente.
+		this.
 		
 		title = new FlxText(0, FlxG.height / 4, 0, "Oship", 12, true);
 		title.screenCenter(X);
