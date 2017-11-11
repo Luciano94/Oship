87830908aee983bc9459b1ab846a5629dda96e9f
Unknown <lucho.elp40@gmail.com>
2017-11-11 13:50:43 -0300

    Intento de barco

--- source/entities/player/PlayerOne.hx
+++ source/entities/player/PlayerOne.hx
@@ -1,8 +1,11 @@
 package entities.player;
 
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
 import flixel.util.FlxColor;
 import flixel.FlxSprite;
 import flixel.FlxG;
+import flixel.math.FlxVelocity;
 
 /**
  * ...
@@ -11,14 +14,12 @@ import flixel.FlxG;
 
 class PlayerOne extends Player 
 {
-	private var speed:Int;
-
+	
 	public function new(?X:Float=0, ?Y:Float=0) 
 	{
 		super(X, Y);
 		//Init
 		makeGraphic(16, 32, FlxColor.RED);
-		speed = Reg.speed;
 	}
 	
 	override public function update(elapsed:Float):Void 
@@ -26,4 +27,42 @@ class PlayerOne extends Player
 		super.update(elapsed);
 	}
 	
+	override function movement():Void 
+	{
+		if (FlxG.keys.pressed.UP)
+		{
+			
+		}
+		if (FlxG.keys.pressed.DOWN)
+		{
+			velocity.x -= 1;
+		}
+		if (FlxG.keys.pressed.RIGHT)
+		{
+			angularVelocity -= 1;
+		}
+		if (FlxG.keys.pressed.LEFT)
+		{
+			angularVelocity += 1;
+		}
+		if (breck())
+		{
+			if (velocity.x > 0)
+				velocity.x  -= 1;
+			if (velocity.x < 0)
+				velocity.x +=  1;
+			if (angularVelocity > 0)
+				angularVelocity -= 1;
+			if (angularVelocity < 0)
+				angularVelocity += 1;
+		}
+	}
+	
+	override function breck():Bool //retorna verdadero cuando una tecla no esta presionada
+	{
+		if (!(FlxG.keys.pressed.UP) && !(FlxG.keys.pressed.DOWN) 
+		&& !(FlxG.keys.pressed.RIGHT) && !(FlxG.keys.pressed.LEFT))
+			return true;
+		return false;
+	}
 }
--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1,12 +1,19 @@
 package source.states;
 
+import entities.player.Player;
+import entities.player.PlayerOne;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	private var playerOne:Player;
+	private var playerTwo:Player;
 	override public function create():Void
 	{
 		super.create();
+		playerOne = new PlayerOne(100, 100);
+		add(playerOne);
+		
 	}
 
 	override public function update(elapsed:Float):Void
