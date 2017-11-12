ea01483e1fe0b8be0b17d7ab1202c6e144e643b5
Unknown <tmugetti@gmail.com>
2017-11-11 21:01:53 -0300

    Weas

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -9,6 +9,7 @@ import flixel.FlxG;
 import AssetPaths;
 import flixel.util.FlxSpriteUtil;
 import states.OptionSubState;
+import openfl.system.System;
 
 class MenuState extends FlxState
 {
@@ -17,11 +18,15 @@ class MenuState extends FlxState
 	private var optionButton:FlxButton;
 	private var fondo:FlxSprite;
 	private var logo:FlxSprite;
+	private var exitButton:FlxButton;
+	private var storeButton:FlxButton;
 	override public function create():Void
 	{
 		super.create();
+		
 		camera = new FlxCamera(0, 0, 1920, 1080);
 		camera.bgColor = 0x00000000;
+		
 		fondo = new FlxSprite();
 		fondo.loadGraphic(AssetPaths.StartMenu__png);
 		fondo.updateHitbox();
@@ -32,20 +37,37 @@ class MenuState extends FlxState
 		
 		logo = new FlxSprite();
 		logo.loadGraphic(AssetPaths.Logo__png);
-		logo.alpha = 0;
 		logo.scale.set(1.5, 1.2);
 		logo.updateHitbox();
 		add(logo);
-		FlxSpriteUtil.fadeIn(logo, 4, false);
 		
-		playButton = new FlxButton(0, 0, "Play", clickPlay);
-		playButton.screenCenter();
+		playButton = new FlxButton("", clickPlay);
+		playButton.loadGraphic(AssetPaths.jugar__png);
+		playButton.updateHitbox();
+		playButton.x = 50;
+		playButton.y = logo.y + logo.height + playButton.height;
 		add(playButton);
 		
-		optionButton = new FlxButton("Opciones",clickOption);
-		optionButton.screenCenter();
-		optionButton.y += playButton.height;
+		optionButton = new FlxButton("", clickOption);
+		optionButton.loadGraphic(AssetPaths.Opciones__png);
+		optionButton.updateHitbox();
+		optionButton.x = 50;
+		optionButton.y = playButton.y + playButton.height;
 		add(optionButton);
+		
+		exitButton = new FlxButton("", salir);
+		exitButton.loadGraphic(AssetPaths.salir__png);
+		exitButton.updateHitbox();
+		exitButton.x = camera.width - exitButton.width - 50;
+		exitButton.y = camera.height - exitButton.height - 50;
+		add(exitButton);
+		
+		storeButton = new FlxButton("",clickStore);
+		storeButton.loadGraphic(AssetPaths.tienda__png);
+		storeButton.updateHitbox();
+		storeButton.x = 50;
+		storeButton.y = optionButton.y + optionButton.height;
+		add(storeButton);
 	}
 
 	override public function update(elapsed:Float):Void
@@ -61,5 +83,13 @@ class MenuState extends FlxState
 	private function clickOption():Void
 	{
 		openSubState(new OptionSubState());
+	}
+	private function salir():Void
+	{
+		System.exit(0);
+	}
+	private function clickStore():Void
+	{
+		
 	}
 }
--- source/states/OptionSubState.hx
+++ source/states/OptionSubState.hx
@@ -1,5 +1,6 @@
 package states;
 
+import flixel.FlxSprite;
 import flixel.FlxSubState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
@@ -14,8 +15,8 @@ class OptionSubState extends FlxSubState
 {
 	private var musicVal:Float;
 	private var sfxVal:Float;
-	private var musicText:FlxText;
-	private var sfxText:FlxText;
+	private var musicText:FlxSprite;
+	private var sfxText:FlxSprite;
 	private var musicValText:FlxText;
 	private var sfxValText:FlxText;
 	private var musMas:FlxButton;
@@ -23,46 +24,59 @@ class OptionSubState extends FlxSubState
 	private var sfxMen:FlxButton;
 	private var sfxMas:FlxButton;
 	private var volver:FlxButton;
-	public function new(BGColor:FlxColor=0x33000000) 
+	public function new(BGColor:FlxColor=0x55FFFFFF) 
 	{
 		super(BGColor);
-		
 		musicVal = FlxG.sound.music.volume * 10;
 		sfxVal = FlxG.sound.volume * 10;
-		musicText = new FlxText("Música: ");
-		sfxText = new FlxText("Maestro:");
-		musicValText = new FlxText(Std.string(musicVal));
-		sfxValText = new FlxText(Std.string(sfxVal));
-		musMas = new FlxButton("+",masVolMus);
-		musMen = new FlxButton("-",menVolMus);
-		sfxMas = new FlxButton("+",masVolSfx);
-		sfxMen = new FlxButton("-",menVolSfx);
-		volver = new FlxButton("Volver", salir);
+		musicText = new FlxSprite();
+		sfxText =  new FlxSprite();
+		musicValText = new FlxText(0,0,0,Std.string(musicVal),64);
+		sfxValText = new FlxText(0,0,0,Std.string(sfxVal),64);
+		musMas = new FlxButton("",masVolMus);
+		musMen = new FlxButton("",menVolMus);
+		sfxMas = new FlxButton("",masVolSfx);
+		sfxMen = new FlxButton("",menVolSfx);
+		volver = new FlxButton("", salir);
+		
+		sfxText.loadGraphic(AssetPaths.maestro__png);
+		sfxText.updateHitbox();
+		musicText.loadGraphic(AssetPaths.musica__png);
+		musicText.updateHitbox();
+		musMas.loadGraphic(AssetPaths.mas__png);
+		musMas.updateHitbox();
+		musMen.loadGraphic(AssetPaths.menos__png);
+		musMen.updateHitbox();
+		sfxMas.loadGraphic(AssetPaths.mas__png);
+		sfxMas.updateHitbox();
+		sfxMen.loadGraphic(AssetPaths.menos__png);
+		sfxMen.updateHitbox();
+		volver.loadGraphic(AssetPaths.volver__png);
+		volver.updateHitbox();
 		
 		sfxText.screenCenter();
 		sfxText.y -= sfxText.height;
-		sfxText.x -= sfxText.width;
+		sfxText.x -= sfxText.width/2;
 		sfxValText.y = sfxText.y;
 		sfxValText.x = sfxText.x + sfxText.width + sfxValText.width;
 		sfxMas.y = sfxText.y;
 		sfxMas.x = sfxValText.x + sfxValText.width;
 		sfxMen.y = sfxText.y;
-		sfxMen.x = sfxMas.x + sfxMas.width;
+		sfxMen.x = sfxMas.x + (sfxMas.width*2);
+		
 		
 		musicText.screenCenter();
 		musicText.y += musicText.height;
-		musicText.x -= musicText.width;
+		musicText.x -= musicText.width/2;
 		musicValText.y = musicText.y;
 		musicValText.x = musicText.x + musicText.width + musicValText.width;
 		musMas.y = musicText.y;
 		musMas.x = musicValText.x + musicValText.width;
 		musMen.y = musicText.y;
-		musMen.x = musMas.x + musMas.width;
+		musMen.x = musMas.x + (musMas.width*2);
 		
 		volver.screenCenter();
-		volver.y = musicText.y + (2 * volver.height);
-		
-		
+		volver.y = musicText.y + (volver.height*2);
 		
 		add(sfxText);
 		add(sfxValText);
@@ -75,9 +89,8 @@ class OptionSubState extends FlxSubState
 		add(musMen);
 		
 		add(volver);
-		
-		
 	}
+	
 	private function masVolMus():Void
 	{
 		if (musicVal <10) 
