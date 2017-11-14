87abd87d39cc62b6c91f2749ca5b25b14c253a6d
Unknown <tmugetti@gmail.com>
2017-11-14 20:47:18 -0300

    Watery bois

--- source/states/BattleState.hx
+++ source/states/BattleState.hx
@@ -1,5 +1,6 @@
 package states;
 
+import flixel.FlxSprite;
 import flixel.FlxSubState;
 import flixel.util.FlxColor;
 import entities.animaciones.SpriteMoneda;
@@ -21,14 +22,42 @@ class BattleState extends FlxSubState
 	private var bulletOne: BulletOne;
 	private var bulletTwo: BulletTwo;
 	private var moneda:SpriteMoneda;
-	
+	private var oceano:Array<Array<FlxSprite>>;
+	private var interfaz:Interfaz;
 	public function new(BGColor:FlxColor=FlxColor.BLUE) 
 	{
 		super(BGColor);
+		oceano = new Array<Array<FlxSprite>>();
+		for (i in 0 ... (Std.int(FlxG.camera.width/128)) +1) 
+		{
+			var linea = new Array<FlxSprite>();
+			for (j in 0 ... (Std.int(FlxG.camera.height/128)) +1 ) 
+			{
+				var ola = new FlxSprite();
+				ola.loadGraphic(AssetPaths.tileOceano__png,true,32,32);
+				ola.animation.add("wave", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], 8, true);
+				ola.animation.play("wave");
+				ola.scale.set(4,4);
+				ola.x = i*128;
+				ola.y = j*128;
+				linea.push(ola);
+			}
+			oceano.push(linea);
+		}
+		for (k in 0 ... (Std.int(FlxG.camera.width/128)) +1) 
+		{
+			for (l in 0 ... (Std.int(FlxG.camera.height/128)) +1) 
+			{
+				add(oceano[k][l]);
+			}
+		}
+		
 		playerOne = new PlayerOne(500, 500, this);
 		playerTwo = new PlayerTwo(700, 700, this);
 		add(playerOne);
 		add(playerTwo);
+		interfaz = new Interfaz(playerOne, this);
+		add(interfaz);
 	}
 	
 	override public function update(elapsed:Float):Void
